55318196adf242037e2d0d9c95db7e6e9b087d00
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2026-02-20 11:32:05 +0600

    Cashout functionality

---
+++ script/cashout.js
@@ -0,0 +1,40 @@
+document.getElementById("cashout-btn").addEventListener("click", function () {
+    // 1. get the agent number and validate
+    const cashoutNumberInput = document.getElementById("cashout-number");
+    const cashoutNumber = cashoutNumberInput.value;
+    console.log(cashoutNumber);
+    if (cashoutNumber.length != 11) {
+        alert("Invalid agent number");
+        return;
+    }
+
+    // 2. get the amount, validate, convert to number
+    const cashoutAmountInput = document.getElementById("cashout-amount");
+    const cashoutAmount = cashoutAmountInput.value;
+    console.log(cashoutAmount);
+
+    // 3. get the current balance, validate, convert to number
+    const balanceElement = document.getElementById("balance");
+    const balance = balanceElement.innerText;
+    console.log(balance);
+
+    // 4. calculate new balance
+    const newBalance = Number(balance) - Number(cashoutAmount);
+    if (newBalance < 0) {
+        alert("Invalid Amount");
+        return;
+    }
+
+    // 5. get the pin and verify
+    const cashoutPinInput = document.getElementById("cashout-pin");
+    const pin = cashoutPinInput.value;
+    if (pin === '1234') {
+        alert("Cashout Successful");
+        console.log(newBalance);
+        balanceElement.innerText = newBalance;
+    }
+    else {
+        alert("Invalid pin");
+        return;
+    }
+})
